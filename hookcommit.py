dbc1bbd8b168ff260bcbcd19b56b5cd54f6aaf01
franeum <elettrochuck@gmail.com>
2022-02-14 12:24:08 +0100

    update

--- hookcommit.py
+++ hookcommit.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 from datetime import datetime
 
 path = './logger.txt'
 
 with open(path, 'a') as logger:
-    logger.write(f"{datetime.now()} committed!\n")
+    logger.write(f"{datetime.now()} committed on {os.uname().machine}\n")
 
 
 
