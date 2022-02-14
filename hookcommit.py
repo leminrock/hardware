6bacb5190ba9288f484817d9f1846b04318c7552
franeum <elettrochuck@gmail.com>
2022-02-14 15:55:58 +0100

    update

--- hookcommit.py
+++ hookcommit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
+import subprocess as sp
 from datetime import datetime
 
 path = './logger.txt'
@@ -9,4 +11,9 @@ with open(path, 'a') as logger:
     logger.write(f"{datetime.now()} committed on {os.uname().machine}\n")
 
 
+source = './network_button/'
+dest = '/etc/systemd/system/'
+filename = 'button_network.service'
 
+#shutil.copyfile(source+filename, dest+filename)
+sp.run(f"sudo cp {source + filename} {dest + filename}".split())
