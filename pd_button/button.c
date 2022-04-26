7fae720b094e4c11a225d3351a94e971cc207e2b
franeum desk <elettrochuck@gmail.com>
2022-04-26 18:32:21 +0200

    clean code

--- pd_button/button.c
+++ pd_button/button.c
@@ -8,10 +8,6 @@
 /* mraa header */
 #include "mraa/gpio.h"
 
-/* gpio declaration */
-#define GPIO_PIN_1 8
-#define DEBOUNCE 4000
-
 static t_class* button_class;
 
 typedef struct _button {
@@ -20,15 +16,14 @@ typedef struct _button {
     mraa_result_t status;
     mraa_gpio_context gpio_1;
     int counter;
-    clock_t start_t;
     int pin;
     int old_value;
 } t_button;
 
-void int_handler(void* args);
+void interrupt_handler_routine(void* args);
 
 // callback func
-void int_handler(void* args)
+void interrupt_handler_routine(void* args)
 {
     t_button* x = (t_button*)args;
 
@@ -36,7 +31,7 @@ void int_handler(void* args)
 
     if (value != x->old_value) {
         if (!value && x->old_value) {
-            post("PRESSED %d TIMES", ++x->counter);
+            post("PRESSED %d TIMES ON PIN %d", ++x->counter, x->pin);
         }
         /*
         else {
@@ -59,13 +54,8 @@ void sig_handler(t_button* x, t_int signum)
 
 void button_free(t_button* x)
 {
-
-    /* release gpio's */
     x->status = mraa_gpio_isr_exit(x->gpio_1);
     x->status = mraa_gpio_close(x->gpio_1);
-
-    //! [Interesting]
-    /* deinitialize mraa for the platform (not needed most of the times) */
     mraa_deinit();
 }
 
@@ -82,29 +72,27 @@ void* button_new(t_floatarg f)
     /* initialize mraa for the platform (not needed most of the times) */
     mraa_init();
 
-    //! [Interesting]
-    /* initialize GPIO pin */
-    x->gpio_1 = mraa_gpio_init((int)f);
+    int pin_number = (int)f;
+    x->gpio_1 = mraa_gpio_init(pin_number);
 
     if (x->gpio_1 == NULL) {
-        post("Failed to initialize GPIO %d", (int)f);
+        post("Failed to initialize GPIO %d", pin_number);
         mraa_deinit();
     }
 
-    /* set GPIO to output */
+    // set GPIO to input
     x->status = mraa_gpio_dir(x->gpio_1, MRAA_GPIO_IN);
+
     if (x->status != MRAA_SUCCESS)
         post("ERROR ON SETTING DIRECTION");
-    else
-        post("RIGHT DIRECTION");
 
     x->status
-        = mraa_gpio_isr(x->gpio_1, MRAA_GPIO_EDGE_BOTH, &int_handler, (t_button*)x);
+        = mraa_gpio_isr(x->gpio_1, MRAA_GPIO_EDGE_BOTH, &interrupt_handler_routine, (void*)x);
+
     if (x->status != MRAA_SUCCESS)
         post("WRONG ISR BINDING");
 
     x->counter = 0;
-    x->start_t = clock();
     x->pin = (int)f;
     x->old_value = -1;
     post("PIN NUMBER IS %d", x->pin);
@@ -115,7 +103,5 @@ void* button_new(t_floatarg f)
 
 void button_setup(void)
 {
-    // button_class = class_new(gensym("button"), (t_newmethod)button_new, (t_method)button_free, sizeof(t_button), CLASS_DEFAULT, 0);
     button_class = class_new(gensym("button"), (t_newmethod)button_new, (t_method)button_free, sizeof(t_button), CLASS_DEFAULT, A_DEFFLOAT, 0);
-    // class_addbang(button_class, button_bang);
 }
